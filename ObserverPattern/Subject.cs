117f72b228c626b0163b5640dfed379561204f6d
Xiaofeng Steve Chiu <cs_qxf@126.com>
2012-07-10 20:07:05 +0800

    Obeserver Pattern Practice

---
+++ ObserverPattern/Subject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ObserverPattern
+{
+    //Subject abstrct parent class
+    abstract class Subject
+    {
+        //Composition of the Observers
+        private List<IObserver> observers = new List<IObserver>();
+
+        //Subject status
+        private int maxTemp;
+        private int minTemp;
+        
+        //Constructor
+        public Subject(int mx, int mn)
+        {
+            this.maxTemp = mx;
+            this.minTemp = mn;
+        }
+
+        //Register an Observer 
+        public void Register(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        //Remove an Observer
+        public void Remove(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        //Update the Observers
+        public void Inform()
+        {
+            foreach(IObserver o in observers)
+                o.Update(this);
+        }
+   
+        //Modifying the status of the Subject
+        public void UpdateData(int mx, int mn)
+        {
+            this.maxTemp = mx;
+            this.minTemp = mn;
+            this.Inform();
+        }
+
+        //Getter for maxtemp
+        public int MaxTemp
+        {
+            get { return maxTemp;}
+        }
+
+        //Getter for mintemp
+        public int MinTemp
+        {
+            get { return minTemp;}
+        }
+    }
+}
+ 
